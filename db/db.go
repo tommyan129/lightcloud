f0bfdf1335ac6fe6d3ea7fb86478031cb0b076a7
unknown <junseeku@gmail.com>
2026-04-21 17:41:17 +0900

    feat:add db tables

--- db/db.go
+++ db/db.go
@@ -10,12 +10,11 @@ import (
 )
 
 const dblogfilepath = "./log/dblog.log"
-const dbfilepath = "./data/user.db"
+const dbfilepath = "./data/lightcloud.db"
 
 var DB *sql.DB
 
 func DBInit() {
-
 	fmt.Println("DBInit 시작")
 	os.MkdirAll("./log", 0755)
 	os.MkdirAll("./data", 0755)
@@ -37,19 +36,53 @@ func DBInit() {
 		log.Fatalf("DB 연결 실패: %v\n", err)
 	}
 
-	createTableQuery := `
+
+	createTableQueries := []string{
+	`
 	CREATE TABLE IF NOT EXISTS users(
 		ID TEXT PRIMARY KEY,
 		Username TEXT NOT NULL,
 		Role TEXT NOT NULL,
 		PasswordHash TEXT NOT NULL,
 		CreatedAt TEXT NOT NULL
-	);`
-
-	_, err = DB.Exec(createTableQuery)
-	if err != nil {
-		log.Fatalf("테이블 생성 실패: %v\n", err)
+	)`,
+	`
+	CREATE TABLE IF NOT EXISTS files(
+	ID TEXT PRIMARY KEY,
+	OwnerID TEXT NOT NULL,
+	OriginalName TEXT NOT NULL,
+	StoredName TEXT NOT NULL,
+	Size INTEGER NOT NULL,
+	MimeType TEXT NOT NULL,
+	CreatedAt TEXT NOT NULL
+	)`,
+	`
+	CREATE TABLE IF NOT EXISTS sessions(
+	Token TEXT PRIMARY KEY,
+	UserID TEXT NOT NULL,
+	ExpiresAt TEXT NOT NULL,
+	CreatedAt TEXT NOT NULL
+	)`,
+	`
+	CREATE TABLE IF NOT EXISTS share_links(
+	Token TEXT PRIMARY KEY,
+	FileID TEXT NOT NULL,
+	CreatedBy TEXT NOT NULL,
+	CreatedAt TEXT NOT NULL,
+	ExpiresAt TEXT NOT NULL,
+	PasswordHash TEXT
+	)`,
 	}
+	
+	for _,q := range createTableQueries{
+		_, err = DB.Exec(q)
+		if err != nil {
+		log.Fatalf("테이블 생성 실패 [%s]: %v\n", q[:30], err)
+	}
+	}
+
+	
+	
 
 	log.Println("DB 초기 설정 완료")
 }
